0afc593a197a7a11ef4796e0ebc5813a7bc1f6a5
rtnpro <rtnpro@transifex.com>
2012-10-14 01:59:52 +0530

    Jotted down some skeleton models to reflect google places data.

--- healthquery/people/models.py
+++ healthquery/people/models.py
@@ -4,11 +4,63 @@ from django.contrib.auth.models import User
 from healthquery.places.models import Place
 
 
+class SpecializationManager(models.ModelManager):
+    def get_by_name_or_alias(self, name):
+        if name is None:
+            raise Specialization.DoesNotExist("No specialization matched "
+                "this query.")
+        specialization = cache.get('specialization:name_or_alias:%s' % name,
+            None)
+        if specialization is None:
+            try:
+                specialization = Specialization.objects.get(
+                    Q(name=name) | Q(aliases__contains='%s,' % name))
+            except Specialization.DoesNotExist, e:
+                pass
+        return specialization
+
+    def for_disease(self, disease):
+        return self.filter(diseases=disease)
+
+
+class Specialization(models.Model):
+    name = models.CharField(verbose_name=u'name', help_text='Specialization'
+        ' of a doctor.')
+    aliases = models.CharField(verbose_name=u'aliases', help_text='A comma '
+        'separated aliases for a specialization')
+    diseases = models.ManyToManyField(Disease, verbose_name=u'diseases',
+        help_text='Diseases that this specialization can handle.')
+
+    objects = SpecializationManager()
+
+    class Meta:
+        verbose_name = _('Specialization')
+
+        verbose_name_plural = _('Specializations')
+
+
+    def __unicode__(self):
+        pass
+            
+
+class DoctorManager(models.ModelManager):
+
+    def for_disease(self, disease):
+        return self.filter(
+            specialization__in=Specialization.objects.for_disease(disease))
+
+    def with_specialization(self, specialization):
+        return self.filter(specialization=specialization)
+
+
 class Doctor(models.Model):
     user = models.OneToOneField(User, blank=True, null=True)
     registration_id = models.CharField(max_length=20)
     places = models.ManyToManyField(Place, through="DoctorLocation",
             blank=True, null=True)
+    specialization = models.ManyToManyField(Specialization)
+
+    objects = DoctorManager()
 
 
 class DoctorLocation(models.Model):
--- healthquery/places/models.py
+++ healthquery/places/models.py
@@ -13,6 +13,60 @@ class Place(BaseAddressModel):
     )
     name = models.CharField(verbose_name=_("name"), max_length=100)
     place_type = models.CharField(max_length="2", choices=CHOICES)
+    formatted_address = models.CharField()
+    formatted_phone_number = models.CharField()
+    international_phone_number = models.CharField()
+    vicinity = models.CharField()
+    icon = models.URLField()
+    types = models.CharField()
+    url = models.URLField()
+    website = models.URLField()
+
+    # ranking to be synced with Google places
+    rating = models.FloatField(verbose_name='rating', help_text='Google'
+        ' Places rating.')
+    google_places_refid = models.CharField(verbose_name=u'google places '
+        'reference id', help_text='Reference id for this place in '
+        'Google places.')
+    google_places_id = models.CharField()
 
     class Meta:
         unique_together = ('name', 'place_type', 'zipcode', 'state', 'country')
+
+
+class Reviews(models.Model):
+    author_name = models.CharField(verbose_name=u'Author name',
+        help_text='Author name')
+    author_url = models.CharField()
+    text = models.TextField()
+    time = models.DateTimeField()
+    aspect_rating = models.FloatField()
+    aspect_type = models.CharField()
+    place = models.ForeignKey(Place)
+
+    class Meta:
+        verbose_name = _('Reviews')
+
+        verbose_name_plural = _('Reviewss')
+
+
+    def __unicode__(self):
+        pass
+
+
+class Event(models.Model):
+    event_id = models.CharField()
+    start_time = models.DateTimeField()
+    summary = models.TextField()
+    url = models.URLField()
+    
+    class Meta:
+        verbose_name = _('Event')
+
+        verbose_name_plural = _('Events')
+
+
+    def __unicode__(self):
+        pass
+    
+            
